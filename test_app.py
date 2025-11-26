e63dd922a2716ff0ffaf45fa2640a67a6683d38f
shimi1717-beep <shimi1717@gmail.com>
2025-11-26 13:50:40 +0200

    Fix test syntax

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
-print(from app import app
+from app import app
 
 def test_root():
     client = app.test_client()
     resp = client.get("/")
     assert resp.status_code == 200
-    assert b"Hello" in resp.data)
+    assert b"Hello" in resp.data
